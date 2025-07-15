4a65d1be9e9248ce9cbfb1bd004f9f5ad35aea32
Nicole260205 <nicolecoelho26@gmail.com>
2025-07-15 16:53:14 -0300

    Arrumando o merch

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -1,23 +1,36 @@
-import blusinha from '../assets/blusinha.png'
-import garrafinha from '../assets/garrafinha.png'
-import bone from '../assets/bone.png'
-import vinil from '../assets/vinil_merch.png'
+import blusinha from "../assets/blusinha.png";
+import garrafinha from "../assets/garrafinha.png";
+import bone from "../assets/bone.png";
+import vinil from "../assets/vinil_merch.png";
 
 function Merch() {
   return (
-    <>
-      <div className="merch">
-        <div className="merch-title">
-          <h1>Merch</h1>
+    <div className="merch">
+      <div className="merch-title">
+        <h1>Merch</h1>
+      </div>
+
+      <div className="merch-grid">
+        <div className="merch-left">
+          <div className="merch-lateral">
+            <img src={bone} alt="boné" />
+            <img src={garrafinha} alt="garrafinha" />
+          </div>
+          <div className="merch-shop">
+            <img src={vinil} alt="vinil" />
+            <div className="merch-shop-text">
+              <h1>HS Merch</h1>
+              <button className="merch-shop-button">shop now</button>
+            </div>
+          </div>
         </div>
-        <div className="merch-grid">
-          <img src={bone} alt="" />
-          <img src={garrafinha} alt="" />
-          <img src={blusinha} alt="" />
+
+        <div className="merch-horizontal">
+          <img src={blusinha} alt="blusinha" />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
-export default Merch
+export default Merch;
