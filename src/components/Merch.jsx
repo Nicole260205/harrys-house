e3a2fe551c0bfbeaa4b5d7d5e2067b2763353bb4
Nicole260205 <nicolecoelho26@gmail.com>
2025-07-15 15:34:07 -0300

    Arrumando o merch

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import blusinha from '../assets/blusinha.png'
+import garrafinha from '../assets/garrafinha.png'
+import bone from '../assets/bone.png'
+import vinil from '../assets/vinil_merch.png'
 
 function Merch() {
   return (
     <>
       <div className="merch">
         <div className="merch-title">
-            <h1></h1>
+          <h1>Merch</h1>
+        </div>
+        <div className="merch-grid">
+          <img src={bone} alt="" />
+          <img src={garrafinha} alt="" />
+          <img src={blusinha} alt="" />
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Merch
